6e32d56cdaaee84c87a8bc5d5b7351675eda405e
karma9874 <33444666+karma9874@users.noreply.github.com>
2020-10-22 19:55:27 +0530

    removed noaction bar

--- app/src/main/java/com/application/kiit_tnp/views/SplashScreen.java
+++ app/src/main/java/com/application/kiit_tnp/views/SplashScreen.java
@@ -34,7 +34,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
         hideSystemUI();
         SQLiteDatabase dbread = new dbHelper(getApplicationContext()).getReadableDatabase();
--- app/src/main/java/com/application/kiit_tnp/views/mainView.java
+++ app/src/main/java/com/application/kiit_tnp/views/mainView.java
@@ -38,7 +38,6 @@ public class mainView extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main_view);
 
         null_data = findViewById(R.id.null_data);
