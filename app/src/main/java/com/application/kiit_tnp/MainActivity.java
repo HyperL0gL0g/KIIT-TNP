53bf05548fb8cd516a68c3291028ffd32268aaac
karma9874 <33444666+karma9874@users.noreply.github.com>
2020-10-23 21:19:24 +0530

    updated login UI

--- app/src/main/java/com/application/kiit_tnp/MainActivity.java
+++ app/src/main/java/com/application/kiit_tnp/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -34,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         final EditText user,pass;
         Button login;
